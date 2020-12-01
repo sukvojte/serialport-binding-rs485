8d6971e6d00fcb5951969c464213d5bbd9d1ed08
Vojtech Suk <sukvojte@gmail.com>
2020-12-01 12:24:36 +0100

    init

---
+++ src/serialport_linux.cpp
@@ -0,0 +1,67 @@
+#if defined(__linux__)
+
+#include <sys/ioctl.h>
+#include <asm/ioctls.h>
+#include <asm/termbits.h>
+#include <linux/serial.h>
+
+// Uses the termios2 interface to set nonstandard baud rates
+int linuxSetCustomBaudRate(const int fd, const unsigned int baudrate) {
+    struct termios2 t;
+
+    if (ioctl(fd, TCGETS2, &t)) {
+      return -1;
+    }
+
+    t.c_cflag &= ~CBAUD;
+    t.c_cflag |= BOTHER;
+    t.c_ospeed = t.c_ispeed = baudrate;
+
+    if (ioctl(fd, TCSETS2, &t)) {
+      return -2;
+    }
+
+    return 0;
+}
+
+// Uses termios2 interface to retrieve system reported baud rate
+int linuxGetSystemBaudRate(const int fd, int* const outbaud) {
+  struct termios2 t;
+
+  if (ioctl(fd, TCGETS2, &t)) {
+    return -1;
+  }
+
+  *outbaud = static_cast<int>(t.c_ospeed);
+
+  return 0;
+}
+
+int linuxSetRs485(Rs485Config *config) {
+  struct serial_rs485 rs485conf;
+
+  rs485conf.flags &= ~(SER_RS485_ENABLED | SER_RS485_RTS_ON_SEND | SER_RS485_RTS_AFTER_SEND | SER_RS485_RX_DURING_TX);
+
+  if(config->rs485_enabled)
+	  rs485conf.flags |= SER_RS485_ENABLED;
+
+  if(config->rs485_rts_on_send)
+	  rs485conf.flags |= SER_RS485_RTS_ON_SEND;
+
+  if(config->rs485_rts_after_send)
+	  rs485conf.flags |= SER_RS485_RTS_AFTER_SEND;
+
+	rs485conf.delay_rts_before_send = config->rs485_delay_before_send;
+
+	rs485conf.delay_rts_after_send = config->rs485_delay_after_send;
+
+  if(config->rs485_rx_during_send)
+	  rs485conf.flags |= SER_RS485_RX_DURING_TX;
+
+	if (ioctl(fd, TIOCSRS485, &rs485conf) < 0)
+	{
+		return -1;
+	}
+}
+
+#endif
---
+++ src/serialport_linux.h
@@ -0,0 +1,19 @@
+#ifndef PACKAGES_SERIALPORT_SRC_SERIALPORT_LINUX_H_
+#define PACKAGES_SERIALPORT_SRC_SERIALPORT_LINUX_H_
+
+int linuxSetCustomBaudRate(const int fd, const unsigned int baudrate);
+int linuxGetSystemBaudRate(const int fd, int* const outbaud);
+
+struct Rs485Config {
+    bool rs485_rts_on_send = false;
+    bool rs485_rts_after_send = true;
+    uint8_t rs485_delay_before_send = 0;
+    uint8_t rs485_delay_after_send = 0;
+    bool rs485_rx_during_send = false;
+    bool rs485_enabled = false;
+}
+
+int linuxSetRs485(Rs485Config *config);
+
+#endif  // PACKAGES_SERIALPORT_SRC_SERIALPORT_LINUX_H_
+
